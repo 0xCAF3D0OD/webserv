b6856ba1b075364bbf8773f45ecaf806f6c04271
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-04-25 13:41:09 +0200

    append the buffer to a string
    that doesn't work

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,11 @@ binary_file(void)
 	ssize_t		bytes_read;
 	char		buffer[BS];
 	int			fd_src;
+	std::string all_file;
 	const char *file_path_src = "test/website/favicon.ico";
 	const char *file_path_dst = "destination.ico";
 	const char *file_path_dst_failed = "destination_failed.ico";
+	const char *file_path_dst_string = "destination_string.ico";
 
 	fd_src = open(file_path_src, O_RDONLY);
 	if (fd_src == -1)
@@ -27,17 +29,22 @@ binary_file(void)
 
 	std::ofstream file_dst(file_path_dst, std::ios::trunc | std::ios::binary);
 	std::ofstream file_dst_error(file_path_dst_failed, std::ios::trunc | std::ios::binary);
+	std::ofstream file_dst_string(file_path_dst_string, std::ios::trunc | std::ios::binary);
 
 	do
 	{
 		std::memset(buffer, 0, BS);
 		bytes_read = read(fd_src, buffer, BS_NULL);
+		all_file += buffer; /* that not work */
 		file_dst.write(buffer, bytes_read);
 		file_dst_error << buffer; /* bad pratice because the output is unformatted */
 	} while (bytes_read == BS_NULL);
 
+	file_dst_string.write(all_file.c_str(), all_file.size()); /* not work */
+
 	file_dst.close();
 	file_dst_error.close();
+	file_dst_string.close();
 	close(fd_src);
 }
 
