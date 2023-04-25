76f8a421cac115d36176a86285de020ee4084fa8
Theo Grivel <tgrivel@c1r15s7.42lausanne.ch>
2023-04-25 14:35:25 +0200

    comment and add vector way

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,19 @@
 
 #include <fstream>
 
+#include <vector>
+
 #define BS 10
 #define BS_NULL 9
 
 static void
 binary_file(void)
 {
-	ssize_t		bytes_read;
-	char		buffer[BS];
-	int			fd_src;
-	std::string all_file;
+	ssize_t			  bytes_read;
+	char			  buffer[BS];
+	int				  fd_src;
+	std::string		  all_file_str;
+	std::vector<char> all_file_vec;
 
 	fd_src = open("test/website/favicon.ico", O_RDONLY);
 	if (fd_src == -1)
@@ -26,21 +29,37 @@ binary_file(void)
 	std::ofstream file_dst("destination.ico", std::ios::trunc | std::ios::binary);
 	std::ofstream file_dst_error("destination_failed.ico", std::ios::trunc | std::ios::binary);
 	std::ofstream file_dst_string("destination_string.ico", std::ios::trunc | std::ios::binary);
+	std::ofstream file_dst_vector("destination_vector.ico", std::ios::trunc | std::ios::binary);
 
 	do
 	{
+		/* reset and read the buffer */
 		std::memset(buffer, 0, BS);
 		bytes_read = read(fd_src, buffer, BS_NULL);
-		all_file += buffer; /* that not work */
+
+		/* KO: append to a string */
+		all_file_str += buffer;
+
+		/* OK: append to a vector*/
+		all_file_vec.insert(all_file_vec.end(), buffer, &(buffer[bytes_read]));
+
+		/* OK: write directly into the file */
 		file_dst.write(buffer, bytes_read);
-		file_dst_error << buffer; /* bad pratice because the output is unformatted */
+
+		/* KO: put unformated contents */
+		file_dst_error << buffer;
 	} while (bytes_read == BS_NULL);
 
-	file_dst_string.write(all_file.c_str(), all_file.size()); /* not work */
+	/* KO: write the string content into the file */
+	file_dst_string.write(all_file_str.c_str(), all_file_str.size());
+
+	/* OK: write the vector content into the file */
+	file_dst_vector.write(all_file_vec.data(), all_file_vec.size());
 
 	file_dst.close();
 	file_dst_error.close();
 	file_dst_string.close();
+	file_dst_vector.close();
 	close(fd_src);
 }
 
