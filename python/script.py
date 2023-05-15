78906ac49c270aaa11f96d6ce44d00264ca7bd17
|°_°| <grivel.theo@protonmail.com>
2023-05-15 15:36:40 +0200

    script: print the response

--- python/script.py
+++ python/script.py
@@ -54,6 +54,7 @@ def redirection (url, url_redirection):
     for count, red in enumerate(request_index.history):
         print(f"history[{count}] -- status code: {red.status_code} url: {red.url}")
         print(red.request.headers)
+        print(red.text)
     if request_index.history[0].status_code != 301:
         print_error(f"[KO] : http status code : {request_index.history[0].status_code} / 301")
         exit_code = 1
